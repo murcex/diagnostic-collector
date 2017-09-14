86fb0d32d88d34274198d9381a1fea81269befb6
Robert G <robertg@murcex.net>
2017-09-14 00:16:03 -0700

    Add data collector files
    Adding data collector files, batch two

---
+++ Korekuta/Korekuta/Program.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+using Library;
+using System.Data.SqlClient;
+
+namespace Korekuta
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region App.config & Metadata
+            // Extract App.config
+            // SQL & URI Targets
+            var connectionSQL = ConfigurationManager.ConnectionStrings["SQL"].ToString();
+            var enviromentType = ConfigurationManager.AppSettings["ENV"].ToString();
+            var controllerType = ConfigurationManager.AppSettings["CTRL"].ToString();
+
+            // Machine Name
+            var machineName = System.Environment.MachineName;
+
+            // Build Connection Payload 
+            var connections = ConnectionController.GetConnections(connectionSQL, enviromentType);
+
+            // Setup API Collection One
+            var apiCollectionOneService = connections.Uri_1_ServiceName.ToString();
+            var apiCollectionOneEnvironment = connections.Uri_1_Environment.ToString();
+            var apiCollectionOneDatabaseCapacity = connections.Uri_1_DatabaseCapacity.ToString();
+            var apiCollectionOneServiceAccount = connections.Uri_1_ServiceAccount.ToString();
+            var apiCollectionOneCertificate = connections.Uri_1_Certificate.ToString();
+            var apiCollectionOneReplication = connections.Uri_1_Replication.ToString();
+            var apiCollectionOneSQLAvailability = connections.Uri_1_SQLAvailability;
+
+            // Setup API Collection Two
+            var apiCollectionTwoService = connections.Uri_2_ServiceName.ToString();
+            var apiCollectionTwoEnvironment = connections.Uri_2_Environment.ToString();
+            var apiCollectionTwoDatabaseCapacity = connections.Uri_2_DatabaseCapacity.ToString();
+            var apiCollectionTwoReplication = connections.Uri_2_Replication;
+            var apiCollectionSQLAvailability = connections.Uri_2_SQLAvailability;
+
+            // Setup API Collection Three
+            var apiCollectionThreeService = connections.Uri_3_ServiceName.ToString();
+            var apiCollectionThreeEnvironment = connections.Uri_3_Environment.ToString();
+            var apiCollectionThreeDatabaseCapacity = connections.Uri_3_DatabaseCapacity.ToString();
+            var apiCollectionThreeReplication = connections.Uri_3_Replication;
+            var apiCollectionThreeSQLAvailability = connections.Uri_3_SQLAvailability;
+
+            // Service Logging
+            //ServiceLog.AddEntry(connectionSQL, DateTime.Now, sessionSet, "app.config", "set", "completed");
+
+            #endregion
+
+            #region Execution Controller
+
+            // Execution Controller Methods
+            bool generationOneDataCollection = ExecutionController.Evaluate(connectionSQL, controllerType, machineName);
+
+            #endregion
+
+            #region Data Collection
+
+            if (generationOneDataCollection == true)
+            {
+                // Data Collection Method -- URI One
+                DatabaseCapacity2.DataCollection(apiCollectionOneDatabaseCapacity, connectionSQL, apiCollectionOneService, apiCollectionOneEnvironment);
+                Replication.DataCollection(apiCollectionOneReplication, connectionSQL, apiCollectionOneService, apiCollectionOneEnvironment);
+                SQLAvailability.DataCollection(apiCollectionOneSQLAvailability, connectionSQL, apiCollectionOneService, apiCollectionOneEnvironment);
+
+                // Data Collection Method -- URI Two
+                DatabaseCapacity2.DataCollection(apiCollectionTwoDatabaseCapacity, connectionSQL, apiCollectionTwoService, apiCollectionTwoEnvironment);
+                Replication.DataCollection(apiCollectionTwoReplication, connectionSQL, apiCollectionTwoService, apiCollectionTwoEnvironment);
+                SQLAvailability.DataCollection(apiCollectionSQLAvailability, connectionSQL, apiCollectionTwoService, apiCollectionTwoEnvironment);
+
+                // Data Collection Method -- URI Three
+                DatabaseCapacity2.DataCollection(apiCollectionThreeDatabaseCapacity, connectionSQL, apiCollectionThreeService, apiCollectionThreeEnvironment);
+                Replication.DataCollection(apiCollectionThreeReplication, connectionSQL, apiCollectionThreeService, apiCollectionThreeEnvironment);
+                SQLAvailability.DataCollection(apiCollectionThreeSQLAvailability, connectionSQL, apiCollectionThreeService, apiCollectionThreeEnvironment);
+
+            }
+
+            #endregion
+
+            #region Post Data Collection
+
+            // Add Retention
+
+            // Add Table Count
+
+            #endregion
+
+            #region Deallocation
+
+            if (generationOneDataCollection == true)
+            {
+                // Deallocate Execution Controller
+                ExecutionController.Deallocate(connectionSQL, controllerType, machineName);
+
+            }
+            #endregion
+
+            #region Execution Logging
+
+            if (generationOneDataCollection == true)
+            {
+                ExecutionLog.AddEntry(connectionSQL, DateTime.Now, machineName, controllerType, "pass,completed");
+            }
+
+            else
+            {
+                ExecutionLog.AddEntry(connectionSQL, DateTime.Now, machineName, controllerType, "fail,closed");
+            }
+            #endregion
+        }
+    }
+}
---
+++ Korekuta/Library/ConnectionController/ConnectionController.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace Library
+{ 
+    public class ConnectionController
+    {
+        #region Connection Object
+        public class Connection
+        {
+            // Connections Properties
+            public string Release { get; set; }
+            public string Workflow { get; set; }
+            public string Service { get; set; }
+            public string Environment { get; set; }
+            public string Key { get; set; }
+            public string Value { get; set; }
+        }
+
+        #endregion
+
+        #region Collection Object
+        public class Collection
+        {
+            // URI One
+            public string Uri_1_ServiceName { get; set; }
+            public string Uri_1_Environment { get; set; }
+            public string Uri_1_DatabaseCapacity { get; set; }
+            public string Uri_1_ServiceAccount { get; set; }
+            public string Uri_1_Certificate { get; set; }
+            public string Uri_1_Replication { get; set; }
+            public string Uri_1_SQLAvailability { get; set; }
+
+            // URI Two
+            public string Uri_2_ServiceName { get; set; }
+            public string Uri_2_Environment { get; set; }
+            public string Uri_2_DatabaseCapacity { get; set; }
+            public string Uri_2_Replication { get; set; }
+            public string Uri_2_SQLAvailability { get; set; }
+
+            // URI Three
+            public string Uri_3_ServiceName { get; set; }
+            public string Uri_3_Environment { get; set; }
+            public string Uri_3_DatabaseCapacity { get; set; }
+            public string Uri_3_Replication { get; set; }
+            public string Uri_3_SQLAvailability { get; set; }
+        }
+
+        #endregion
+
+        #region Connection Controller
+        // Return Connections by Enviroment
+        public static Collection GetConnections(string connectionString, string enviromentType)
+        {
+            var connections = new List<Connection>();
+
+            var collection = new Collection();
+
+            using (var connection = new System.Data.SqlClient.SqlConnection(connectionString))
+            {
+                var cmd = new SqlCommand("usp_Article_Connections_Load", connection);
+                cmd.CommandType = System.Data.CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("nvc_release", enviromentType);
+
+                connection.Open();
+
+                var reader = cmd.ExecuteReader();
+                while (reader.Read())
+                {
+                    var item = new Connection();
+                    item.Release = (string)reader["nvc_release"];
+                    item.Workflow = (string)reader["nvc_workflow"];
+                    item.Service = (string)reader["nvc_service"];
+                    item.Environment = (string)reader["nvc_environment"];
+                    item.Key = (string)reader["nvc_key"];
+                    item.Value = (string)reader["nvc_value"];
+                    connections.Add(item);
+                }
+            }
+
+            //// URI One
+            // Service Name & Environment
+            collection.Uri_1_ServiceName = connections.Where(x => x.Workflow == "1").Select(x => x.Service).FirstOrDefault();
+
+            collection.Uri_1_Environment = connections.Where(x => x.Workflow == "1").Select(x => x.Environment).FirstOrDefault();
+            
+            // URI ID's
+            collection.Uri_1_DatabaseCapacity = connections.Where(x => x.Workflow == "1").Select(x => x.Value).FirstOrDefault() + connections.Where(x => x.Service == "DatabaseCapacity").Select(x => x.Value).FirstOrDefault();
+
+            collection.Uri_1_ServiceAccount = connections.Where(x => x.Workflow == "1").Select(x => x.Value).FirstOrDefault() + connections.Where(x => x.Service == "ServiceAcount").Select(x => x.Value).FirstOrDefault();
+
+            collection.Uri_1_Certificate = connections.Where(x => x.Workflow == "1").Select(x => x.Value).FirstOrDefault() + connections.Where(x => x.Service == "Certificate").Select(x => x.Value).FirstOrDefault();
+
+            collection.Uri_1_Replication = connections.Where(x => x.Workflow == "1").Select(x => x.Value).FirstOrDefault() + connections.Where(x => x.Service == "Replication").Select(x => x.Value).FirstOrDefault();
+
+            collection.Uri_1_SQLAvailability = connections.Where(x => x.Workflow == "1").Select(x => x.Value).FirstOrDefault() + connections.Where(x => x.Service == "SQLAvailability").Select(x => x.Value).FirstOrDefault();
+
+            //// URI Two
+            // Service Name & Environment
+            collection.Uri_2_ServiceName = connections.Where(x => x.Workflow == "2").Select(x => x.Service).FirstOrDefault();
+
+            collection.Uri_2_Environment = connections.Where(x => x.Workflow == "2").Select(x => x.Environment).FirstOrDefault();
+            
+            // URI ID's
+            collection.Uri_2_DatabaseCapacity = connections.Where(x => x.Workflow == "2").Select(x => x.Value).FirstOrDefault() + connections.Where(x => x.Service == "DatabaseCapacity").Select(x => x.Value).FirstOrDefault();
+
+            collection.Uri_2_Replication = connections.Where(x => x.Workflow == "2").Select(x => x.Value).FirstOrDefault() + connections.Where(x => x.Service == "Replication").Select(x => x.Value).FirstOrDefault();
+
+            collection.Uri_2_SQLAvailability = connections.Where(x => x.Workflow == "2").Select(x => x.Value).FirstOrDefault() + connections.Where(x => x.Service == "SQLAvailability").Select(x => x.Value).FirstOrDefault();
+
+            //// URI Three
+            // Service Name & Environment
+            collection.Uri_3_ServiceName = connections.Where(x => x.Workflow == "3").Select(x => x.Service).FirstOrDefault();
+
+            collection.Uri_3_Environment = connections.Where(x => x.Workflow == "3").Select(x => x.Environment).FirstOrDefault();
+
+            // URI ID's
+            collection.Uri_3_DatabaseCapacity = connections.Where(x => x.Workflow == "3").Select(x => x.Value).FirstOrDefault() + connections.Where(x => x.Service == "DatabaseCapacity").Select(x => x.Value).FirstOrDefault();
+
+            collection.Uri_3_Replication = connections.Where(x => x.Workflow == "3").Select(x => x.Value).FirstOrDefault() + connections.Where(x => x.Service == "Replication").Select(x => x.Value).FirstOrDefault();
+
+            collection.Uri_3_SQLAvailability = connections.Where(x => x.Workflow == "3").Select(x => x.Value).FirstOrDefault() + connections.Where(x => x.Service == "SQLAvailability").Select(x => x.Value).FirstOrDefault();
+
+            return collection;
+        }
+
+        #endregion
+    }
+}
