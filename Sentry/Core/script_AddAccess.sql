41268359866a9cbf280fa0f7fc7bf168d42b12da
Robert G <robertg@murcex.net>
2017-11-04 16:32:29 -0700

    Remove Old Sentry Folder
    Delete old sentry folder and files

--- Sentry/Core/script_AddAccess.sql
+++
@@ -1,8 +0,0 @@
-CREATE LOGIN [DOMAIN\user]
-FROM WINDOWS WITH DEFAULT_DATABASE = [master]
-	,DEFAULT_LANGUAGE = [us_english]
-GO
-
-EXEC master..sp_addsrvrolemember @loginame = N'DOMAIN\user'
-	,@rolename = N'sysadmin'
-GO
