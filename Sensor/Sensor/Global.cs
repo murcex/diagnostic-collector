ecc494580077b78eac0e975868d5d81b1afc5d94
Robert G <robertg@murcex.net>
2017-11-05 13:33:21 -0800

    Add Global.cs and Rename GetHostNames.cs
    (1) Add Global.cs file, updating solution accordingly
    (2) Renaming GetHostNames.cs to TargetAcquistition.cs, updating solution
    accordingly
    (3) Minor changes to debug WriteLine output

---
+++ Sensor/Sensor/Global.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+
+namespace Sensor
+{
+    public static class Global
+    {
+        public static readonly String SensorLocation = ConfigurationManager.AppSettings["sensor"].ToString();
+        public static readonly String SQLConnectionString = ConfigurationManager.AppSettings["sql"].ToString();
+        public static readonly DateTime SessionDatetime = DateTime.Now.ToUniversalTime();
+    }
+}
