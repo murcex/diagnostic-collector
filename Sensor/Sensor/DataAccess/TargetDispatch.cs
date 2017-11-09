8f210b942b03bec7f89def5a87ab7d8b1b32e7bb
Robert G <robertg@murcex.net>
2017-11-08 23:57:50 -0800

    Sensor - v4 - Update Database Tagging
    (1) Update Sensor object for DataCenter data
    (2) Bind DataCenter logic to Sensor object
    (3) Add DataCenter properties to SQL Upsert
    (4) Add Console reporting for new Sensor properties
    (5) Reorganize classes into folders
    (6) Dummy App.config as xml file

--- Sensor/Sensor/DataAccess/TargetDispatch.cs
+++ Sensor/Sensor/DataAccess/TargetDispatch.cs
@@ -15,25 +15,29 @@ namespace Sensor
             {
                 //if (Global.DebugMode == 1)
                 //{
-                    Console.WriteLine("-- SQL Upsert Check --");
-                    Console.WriteLine("Session: {0}", target.dt_session);
-                    Console.WriteLine("Source: {0}", target.nvc_source);
-                    Console.WriteLine("DNS: {0}", target.nvc_dns);
-                    Console.WriteLine("IPAddress: {0}", target.nvc_ip);
-                    Console.WriteLine("Status: {0}", target.nvc_status);
-                    Console.WriteLine("Latency: {0} \r\n", target.i_latency);
+                Console.WriteLine("-- SQL Upsert Check --");
+                Console.WriteLine("Session: {0}", target.dt_session);
+                Console.WriteLine("Source: {0}", target.nvc_source);
+                Console.WriteLine("DataCenter: {0}", target.nvc_datacenter);
+                Console.WriteLine("DataCenter Tag: {0}", target.nvc_datacentertag);
+                Console.WriteLine("DNS: {0}", target.nvc_dns);
+                Console.WriteLine("IPAddress: {0}", target.nvc_ip);
+                Console.WriteLine("Status: {0}", target.nvc_status);
+                Console.WriteLine("Latency: {0} \r\n", target.i_latency);
                 //}
 
                 using (SqlConnection connection = new SqlConnection(Global.SQLConnectionString))
                 {
                     // SQLCommand & Command Type -- Add SQL Insert Stored Procedure
-                    SqlCommand command = new SqlCommand("usp_Sensor_Staging_Upsert", connection);
+                    SqlCommand command = new SqlCommand("usp_Sensor_Staging_Upsert_v2", connection);
                     command.CommandType = System.Data.CommandType.StoredProcedure;
 
                     // Data Capacity Columns -- example: command.Parameters.AddWithValue("Value1", Value1);
                     // Example: command.Parameters.AddWithValue("execution_runtime", item.execution_runtime);
                     command.Parameters.AddWithValue("dt_session", target.dt_session);
                     command.Parameters.AddWithValue("nvc_source", target.nvc_source);
+                    command.Parameters.AddWithValue("nvc_datacenter", target.nvc_datacenter);
+                    command.Parameters.AddWithValue("nvc_datacentertag", target.nvc_datacentertag);
                     command.Parameters.AddWithValue("nvc_dns", target.nvc_dns);
                     command.Parameters.AddWithValue("nvc_ip", target.nvc_ip);
                     command.Parameters.AddWithValue("nvc_status", target.nvc_status);
