ee380afa12a0a11877ae975750ab4d1e955d8b09
Robert G <robertg@murcex.net>
2018-03-28 02:45:45 -0700

    Dev Commit
    - Add Check and Create folders logic to Template.cs
    - Add Check folders logic to Audit.cs

--- Crane/Crane/Audit.cs
+++ Crane/Crane/Audit.cs
@@ -3,13 +3,32 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace Crane
 {
     class Audit
     {
-        // check source folder and for all object folders
-        // check sql instance connection
-        // check power in sql instance
+        // check power in sql instance ?
+        public static void Build()
+        {
+            // check source folder and for all object folders
+            string path = Directory.GetCurrentDirectory();
+
+            foreach (KeyValuePair<int, string> objectType in Global.ObjectType)
+            {
+                string pathObject = path + @"\" + objectType.Value;
+
+                // create folder struct
+                // Determine whether the directory exists.
+                if (Directory.Exists(pathObject))
+                {
+                    Console.WriteLine("That path exists already.");
+                    return;
+                }
+            }
+
+            // check sql instance connection
+        }
     }
 }
