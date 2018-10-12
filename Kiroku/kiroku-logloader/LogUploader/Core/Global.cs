c7d5676c5b3384050a65180ddccde80783619c7e
Robert G <robertg@murcex.net>
2018-10-11 19:50:07 -0700

    Add Debug ReadKey and remove V1 offline code
    n/a

--- Kiroku/kiroku-logloader/LogUploader/Core/Global.cs
+++ Kiroku/kiroku-logloader/LogUploader/Core/Global.cs
@@ -5,9 +5,20 @@ namespace KLOGLoader
 {
     class Global
     {
+        public static readonly String Debug = ConfigurationManager.AppSettings["debug"].ToString();
         public static readonly String AzureContainer = ConfigurationManager.AppSettings["azureContainer"].ToString();
         public static readonly String SqlConnetionString = ConfigurationManager.AppSettings["sql"].ToString();
         static string _retentionDays = ConfigurationManager.AppSettings["retentionDays"].ToString();
         public static readonly Double RetentionDays = Convert.ToDouble(_retentionDays);
+
+        public static void CheckDebug()
+        {
+            if (Debug == "1")
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("\n\tDEBUG DETECTED, PRESS ANY KEY");
+                Console.ReadKey();
+            }
+        }
     }
 }
