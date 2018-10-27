5779c9712483cc4f6749ffee56475a753700de68
Robert <vr38dett@live.com>
2018-10-26 19:40:26 -0700

    Message Length Check (#32)
    (1) Add MessageLength var to Global
    (2) Add messageLength value to App.config
    (3) Switch Log table to [nvarchar](250) for messages
    (4) Add Log message length check, append error tag and switch type to
    Error

--- Kiroku/kiroku-database/table/tbl_Kiroku_KLogs.sql
+++ Kiroku/kiroku-database/table/tbl_Kiroku_KLogs.sql
@@ -4,5 +4,5 @@ CREATE TABLE [dbo].[tbl_Kiroku_KLogs](
 	[ui_blockid] [uniqueidentifier] NULL,
 	[nvc_blockname] [nvarchar](50) NULL,
 	[nvc_logtype] [nvarchar](15) NULL,
-	[nvc_logdata] [nvarchar](2000) NULL
+	[nvc_logdata] [nvarchar](250) NULL
 ) ON [PRIMARY]
--- Kiroku/kiroku-logloader/LogUploader/Core/Global.cs
+++ Kiroku/kiroku-logloader/LogUploader/Core/Global.cs
@@ -9,7 +9,9 @@ namespace KLOGLoader
         public static readonly String AzureContainer = ConfigurationManager.AppSettings["azureContainer"].ToString();
         public static readonly String SqlConnetionString = ConfigurationManager.AppSettings["sql"].ToString();
         static string _retentionDays = ConfigurationManager.AppSettings["retentionDays"].ToString();
+        static string _messageLength = ConfigurationManager.AppSettings["messageLength"].ToString();
         public static readonly Double RetentionDays = Convert.ToDouble(_retentionDays);
+        public static readonly int MessageLength = Convert.ToInt32(_messageLength);
 
         public static void CheckDebug()
         {
