c08d98dcfe0196ebc14f2341227a3611b4faff58
Robert <vr38dett@live.com>
2018-12-05 22:39:02 -0800

    Adding Implements (#44)
    (1) Deserializer
    (2) ASE Encryption
    (3) Simple Local Logging

---
+++ Implements/Implements/Deserializer/KVP.cs
@@ -0,0 +1,15 @@
+﻿namespace Implements
+{
+    public class KVPModel
+    {
+        /// <summary>
+        /// The Key of the KV Pair.
+        /// </summary>
+        public string A { get; set; }
+
+        /// <summary>
+        /// The Balue of the KV Pair.
+        /// </summary>
+        public string B { get; set; }
+    }
+}
