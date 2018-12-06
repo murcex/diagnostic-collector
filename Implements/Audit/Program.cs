50c9aa33dbe7ea74d1c556304fb9e58f326f810e
Robert G <robertg@murcex.net>
2018-12-05 22:38:36 -0800

    Adding Implements
    (1) Deserializer
    (2) ASE Encryption
    (3) Simple Local Logging

---
+++ Implements/Audit/Program.cs
@@ -0,0 +1,120 @@
+﻿namespace Audit
+{
+    using System;
+    using System.Collections.Generic;
+
+    // Implements
+    using Implements;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ///
+            /// Logging
+            ///
+
+            Log.Initialize();
+            
+            ///
+            /// Deserializer Example
+            ///
+
+            Config cfg = new Config();
+
+            try
+            {
+                using (Deserializer deserializer = new Deserializer())
+                {
+                    cfg.Collection = deserializer.Execute(@"C:\Temp\MyConfig\MyConfigFile.txt", true, true);
+
+                    Console.WriteLine("");
+                    Console.WriteLine("--- Program.cs Check ---");
+
+                    foreach (var item in cfg.Collection)
+                    {
+                        Console.WriteLine("");
+                        Console.WriteLine($"Tag: {item.Key}");
+
+                        foreach (var pair in item.Value)
+                        {
+                            Console.WriteLine($"PartA: {pair.A} = PartB: {pair.B}");
+                        }
+                    }
+                }
+
+                var test1 = cfg.GetValue("app_first", "appname");
+                var test2 = cfg.GetValue("app_second", "type");
+                var test3 = cfg.GetValues("apps_index", "app");
+
+                Console.WriteLine("");
+                Console.WriteLine("Test Value Select:");
+                Console.WriteLine($"Test Select: {test1}");
+                Console.WriteLine($"Test Select: {test2}");
+                Console.WriteLine("");
+                Console.WriteLine("Test Values Select:");
+                foreach (var item in test3)
+                {
+                    Console.WriteLine($"Test Select: {item}");
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Deserializer Exception: {e.ToString()}");
+            }
+
+            try
+            {
+                ///
+                /// Encryption.Encrypt();
+                ///
+
+                var key = "yourstringpasswordhere";
+                var iv = "16charlongivonly";
+                List<string> encrList;
+
+                List<string> myLine = new List<string>();
+                myLine.Add("Test Line 1");
+                myLine.Add("Test Line 2");
+
+                using (Encryption encr_1 = new Encryption(key, iv))
+                {
+
+                    encrList = encr_1.Encrypt(myLine);
+
+                    Console.WriteLine("");
+                    Console.WriteLine("-- Encrypt Test --");
+
+                    foreach (string line in encrList)
+                    {
+                        Console.WriteLine($"Contents: {line}");
+                    }
+                }
+
+                ///
+                /// Encryption.Decrypt();
+                ///
+                
+                using (Encryption encr_2 = new Encryption(key, iv))
+                {
+
+                    var myNewList = encr_2.Decrypt(encrList);
+
+                    Console.WriteLine("");
+                    Console.WriteLine("-- Decrypt Test --");
+
+                    foreach (string line in myNewList)
+                    {
+                        Console.WriteLine($"Contents: {line}");
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Encryption Exception: {e.ToString()}");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Implements/Implements/Deserializer/Deserializer.cs
@@ -0,0 +1,274 @@
+﻿namespace Implements
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+
+    public class Deserializer : IDisposable
+    {
+        /// <summary>
+        /// Flag used by the rule engine to determine if a Tag has been identified.
+        /// </summary>
+        static bool TagFilterSwitch = false;
+
+        /// <summary>
+        /// The current Tag name.
+        /// </summary>
+        static string CurrentTagName;
+
+        /// <summary>
+        /// Disposable flag.
+        /// </summary>
+        bool dispose = false;
+
+        /// <summary>
+        /// Core Deserializer process.
+        /// </summary>
+        /// <param name="fileName"></param>
+        /// <param name="logOperation"></param>
+        /// <param name="logValidation"></param>
+        /// <returns></returns>
+        public Dictionary<string, List<KVPModel>> Execute(string fileName, bool logOperation = false, bool logValidation = false)
+        {
+            if (logOperation || logValidation)
+            {
+                if (!Log.Status)
+                {
+                    throw new Exception($"Log Exception [Log].[Status]: Logger has not been Initialized!");
+                }
+            }
+            if (logOperation)
+            {
+                Log.Info($"File Name: {Path.GetFileName(fileName)}");
+            }
+
+            List<string> lines = new List<string>();
+
+            try
+            {
+                using (StreamReader reader = new StreamReader(fileName))
+                {
+                    while (!reader.EndOfStream)
+                    {
+                        lines.Add(reader.ReadLine());
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                throw new Exception($"Deserializer Exception [Deserializer].[Execute()]: StreamReaders Error: {e.ToString()}");
+            }
+
+            List<string> includesFiles = new List<string>();
+
+            ///
+            /// --- DESERIALIZER RULE ENGINE ---
+            ///
+
+            Dictionary<string, List<KVPModel>> tagCollection = new Dictionary<string, List<KVPModel>>();
+
+            List<KVPModel> tagList = new List<KVPModel>();
+
+            try
+            {
+                foreach (var line in lines)
+                {
+                    if (logOperation)
+                    {
+                        Log.Info("");
+                        Log.Info($"Line Check: {line}");
+                    }
+
+                    if (TagFilterSwitch && line != string.Empty && !line.Contains(";"))
+                    {
+                        if (line.Contains("[") && line.Contains("]"))
+                        {
+                            TagFilterSwitch = true;
+
+                            var tagName = CleanTag(line);
+
+                            tagCollection.Add(CurrentTagName, tagList);
+
+                            tagList = new List<KVPModel>();
+
+                            CurrentTagName = tagName;
+
+                            if (logOperation)
+                            {
+                                Log.Info($"New Tag Dectected: {tagName}");
+                                Log.Info($"Added tagList to tagCollection.");
+                                Log.Info($"Cleared tagList.");
+                                Log.Info($"Update CurrentTag.");
+                            }
+                        }
+                        else
+                        {
+                            var SecondValueSwitch = false;
+
+                            string firstValue = string.Empty;
+                            string secondValue = string.Empty;
+
+                            char checkthis = '=';
+
+                            foreach (var chr in line)
+                            {
+                                if (!SecondValueSwitch)
+                                {
+                                    if (chr == checkthis)
+                                    {
+                                        SecondValueSwitch = true;
+                                    }
+                                    else
+                                    {
+                                        if (firstValue == string.Empty)
+                                        {
+                                            firstValue = chr.ToString();
+                                        }
+                                        else
+                                        {
+                                            firstValue = string.Concat(firstValue, chr.ToString());
+                                        }
+                                    }
+                                }
+                                else
+                                {
+                                    if (secondValue == string.Empty)
+                                    {
+                                        secondValue = chr.ToString();
+                                    }
+                                    else
+                                    {
+                                        secondValue = string.Concat(secondValue, chr.ToString());
+                                    }
+                                }
+                            }
+
+                            KVPModel kvpModel = new KVPModel();
+
+                            kvpModel.A = firstValue;
+                            kvpModel.B = secondValue;
+
+                            tagList.Add(kvpModel);
+
+                            if (logOperation)
+                            {
+                                Log.Info($"Tag Member Detected: {line} -- Adding to {CurrentTagName} list.");
+                                Log.Info($"Part A: {firstValue} Part B: {secondValue}");
+                            }
+                        }
+                    }
+                    else if (!line.Contains(";"))
+                    {
+                        if (line.Contains("[") && line.Contains("]"))
+                        {
+                            TagFilterSwitch = true;
+
+                            var tagName = CleanTag(line);
+
+                            CurrentTagName = tagName;
+
+                            if (logOperation)
+                            {
+                                Log.Info($"Tag Detected: {tagName}");
+                            }
+                        }
+                        else
+                        {
+                            if (logOperation)
+                            {
+                                Log.Info($"No-Hit: {line}");
+                            }
+                        }
+                    }
+                    else
+                    {
+                        if (line.Contains(";"))
+                        {
+                            if (logOperation)
+                            {
+                                Log.Info($"Comment Detected: {line}");
+                            }
+                        }
+                        else
+                        {
+                            if (logOperation)
+                            {
+                                Log.Info($"Unknown Hit: {line}");
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                throw new Exception($"Deserializer Exception [Deserializer].[Execute()]: Rule Engine Error: {e.ToString()}");
+            }
+
+            if (logValidation)
+            {
+                try
+                {
+                    Log.Info($"");
+                    Log.Info($"--- Validation ---");
+
+                    foreach (var tag in tagCollection)
+                    {
+                        Log.Info("");
+                        Log.Info($"Tag: {tag.Key}");
+
+                        foreach (var pair in tag.Value)
+                        {
+                            Log.Info($"A: {pair.A} = B: {pair.B}");
+                        }
+                    }
+                }
+                catch (Exception e)
+                {
+                    throw new Exception($"Deserializer Exception [Deserializer].[Execute()]: Validation Error: {e.ToString()}");
+                }
+            }
+
+            return tagCollection;
+        }
+
+        /// <summary>
+        /// Clean the Tag of brackets.
+        /// </summary>
+        /// <param name="rawTag"></param>
+        /// <returns></returns>
+        private string CleanTag(string rawTag)
+        {
+            var tagName = rawTag.Replace("[", "");
+            tagName = tagName.Replace("]", "");
+
+            return tagName;
+        }
+
+        /// <summary>
+        /// Disposable Logic
+        /// </summary>
+        /// <param name="disposing"></param>
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!dispose)
+            {
+                if (disposing)
+                {
+                    //dispose managed resources
+                }
+            }
+
+            //dispose unmanaged resources
+            dispose = true;
+        }
+
+        /// <summary>
+        /// Dispose process.
+        /// </summary>
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+    }
+}
