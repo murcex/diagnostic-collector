c2ce3527f3557a125427b98efd287e2d623f98ab
Robert <vr38dett@live.com>
2019-07-16 20:55:36 -0700

    Add Dynamic Instance Logging (#59)
    N/A

--- Kiroku/kiroku-library/Kiroku/DataWriters/LogFileWriter.cs
+++ Kiroku/kiroku-library/Kiroku/DataWriters/LogFileWriter.cs
@@ -12,7 +12,7 @@
     /// </summary>
     class LogFileWriter
     {
-        #region Start Instance
+        #region Start Instance (Static)
 
         /// <summary>
         /// Create a new local KLOG file with the instance header as the first line.
@@ -45,7 +45,40 @@
 
         #endregion
 
-        #region Stop Instance
+        #region Start Instance (Dynamic)
+
+        /// <summary>
+        /// Create a new local KLOG file with the instance header as the first line with instance Id.
+        /// </summary>
+        /// <param name="instanceStatus"></param>
+        public static void StartInstanceWithId(string instanceStatus, Guid instance)
+        {
+            try
+            {
+                using (LogInstance logInstance = new LogInstance(instanceStatus, instance))
+                {
+                    var _logInstancestring = JsonConvert.SerializeObject(logInstance);
+
+                    using (StreamWriter file = new StreamWriter(LogConfiguration.FullFilePath + instance.ToString() + ".txt", true))
+                    {
+                        file.WriteLine(LogType.InstanceStatusTag + _logInstancestring);
+                    }
+
+                    if (LogConfiguration.WriteVerbose == "1")
+                    {
+                        LogVerboseWriter.Write(logInstance);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Log.Error($"[LogFileWriter].[StartInstanceWithId] - Exception: {ex.ToString()}");
+            }
+        }
+
+        #endregion
+
+        #region Stop Instance (Static)
 
         /// <summary>
         /// Add the closing instance footer to the local LOG file as the last line.
@@ -76,9 +109,42 @@
             }
         }
 
+        #endregion 
+
+        #region Stop Instance (Dynamic)
+
+        /// <summary>
+        /// Add the closing instance footer to the local LOG file as the last line by instance Id.
+        /// </summary>
+        /// <param name="instanceStatus"></param>
+        public static void StopInstanceWithId(string instanceStatus, Guid instance)
+        {
+            try
+            {
+                using (LogInstance logInstance = new LogInstance(instanceStatus, instance))
+                {
+                    var _logInstancestring = JsonConvert.SerializeObject(logInstance);
+
+                    using (StreamWriter file = File.AppendText(LogConfiguration.FullFilePath + instance.ToString() + ".txt"))
+                    {
+                        file.WriteLine(LogType.InstanceStatusTag + _logInstancestring);
+                    }
+
+                    if (LogConfiguration.WriteVerbose == "1")
+                    {
+                        LogVerboseWriter.Write(logInstance);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Log.Error($"[LogFileWriter].[StopInstanceWithId] - Exception: {ex.ToString()}");
+            }
+        }
+
         #endregion
 
-        #region Add Record
+        #region Add Record (Static)
 
         /// <summary>
         /// Write a single log record to the local KLOG file.
@@ -91,6 +157,31 @@
                 var _logRecordEntry = JsonConvert.SerializeObject(logRecordPayload);
 
                 using (StreamWriter file = File.AppendText(LogConfiguration.FullFilePath))
+                {
+                    file.WriteLine(_logRecordEntry);
+                }
+            }
+            catch (Exception ex)
+            {
+                Log.Error($"[LogFileWriter].[AddRecord] - Exception: {ex.ToString()}");
+            }
+        }
+
+        #endregion
+
+        #region Add Record (Dynamic)
+
+        /// <summary>
+        /// Write a single log record to the local KLOG file with instance Id.
+        /// </summary>
+        /// <param name="logRecordPayload"></param>
+        public static void AddRecord(LogRecord logRecordPayload, Guid instance)
+        {
+            try
+            {
+                var _logRecordEntry = JsonConvert.SerializeObject(logRecordPayload);
+
+                using (StreamWriter file = File.AppendText(LogConfiguration.FullFilePath + instance.ToString() + ".txt"))
                 {
                     file.WriteLine(_logRecordEntry);
                 }
