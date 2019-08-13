22039bb6f6b167a7661d46efa62a64cfd8247cda
Robert G <vr38dett@live.com>
2019-08-12 22:58:25 -0700

    SQL Clean-up
    Remove legacy tables and procs

--- Sensor/sensor-database/proc/usp_Sensor_DNS_Stage_Retention.sql
+++
@@ -1,9 +0,0 @@
-CREATE PROCEDURE [dbo].[usp_Sensor_DNS_Stage_Retention]
-
-AS
-
-BEGIN
-  DELETE TOP (50)
-  FROM [dbo].[tbl_Sensor_DNS_Stage]
-  WHERE [dt_session] < DATEADD(day,-7,GETDATE())
-END
--- Sensor/sensor-database/table/tbl_Sensor_DNSDistribution_Stage.sql
+++
@@ -1,9 +0,0 @@
-IF (OBJECT_ID(N'[dbo].[tbl_Sensor_DNSDistribution_Stage]', 'U') IS NULL)
-BEGIN
-  CREATE TABLE [dbo].[tbl_Sensor_DNSDistribution_Stage]
-  ([dt_session] [smalldatetime] NULL
-	,[nvc_source] [nvarchar](50) NULL
-	,[nvc_dns] [nvarchar](100) NULL
-	,[nvc_ip] [nvarchar](20) NULL
-	,[i_count] [int] NULL)
-END
