dd9c9570c97eae1c1ee2197a19fb2cf3ed0d793d
Robert G <vr38dett@live.com>
2019-10-22 20:23:01 -0700

    Update
    - First draft kquery
    - Add kiroku index

---
+++ Kiroku/kiroku-logquery/KQuery/Core/Global.cs
@@ -0,0 +1,50 @@
+﻿namespace KQuery
+{
+    using Implements;
+    using System.Collections.Generic;
+
+    class Global
+    {
+        static Global()
+        {
+            GetConfigs();
+            SetConfig();
+        }
+
+        public static void GetConfigs()
+        {
+            using (Deserializer deserilaizer = new Deserializer())
+            {
+                var _file = @"D:\home\site\wwwroot\Config.ini";
+
+                deserilaizer.Execute(_file);
+
+                KQueryTagList = deserilaizer.GetTag("kquery");
+
+                //_kirokuTagList = deserilaizer.GetTag("kiroku");
+            }
+        }
+
+        private static void SetConfig()
+        {
+            foreach (var kvp in KQueryTagList)
+            {
+                switch (kvp.Key.ToString())
+                {
+                    case "storage":
+                        StorageConnectionString = kvp.Value;
+                        break;
+
+                    default:
+                        {
+                            //Log.Error($"Not Hit: {kvp.Key}");
+                        }
+                        break;
+                }
+            }
+        }
+
+        private static List<KeyValuePair<string, string>> KQueryTagList { get; set; }
+        public static string StorageConnectionString { get; private set; }
+    }
+}
---
+++ Kiroku/kiroku-logquery/KQuery/Processor/Storage.cs
@@ -0,0 +1,62 @@
+﻿namespace KQuery
+{
+    using Microsoft.WindowsAzure.Storage;
+    using Microsoft.WindowsAzure.Storage.Blob;
+    using System.IO;
+
+    class Storage
+    {
+        public static byte[] GetLog(string key)
+        {
+            byte[] document = null;
+
+            try
+            {
+                // Parse connection string
+                CloudStorageAccount storageAccount = CloudStorageAccount.Parse(Global.StorageConnectionString);
+
+                // Create the blob client
+                CloudBlobClient blobClient = storageAccount.CreateCloudBlobClient();
+
+                // Retrieve container reference -- create if it's not availble
+                CloudBlobContainer container = blobClient.GetContainerReference("kiroku-archive");
+                container.CreateIfNotExistsAsync().GetAwaiter();
+
+                // Build KLOG _R_ead file name to use on the blob write
+                var blobfileName = "KLOG_R_" + key + ".txt";
+
+                // Retrieve blob reference -- example: containerName\$(localDir)\KLOG_R_$(guid).txt
+                CloudBlockBlob blockBlob = container.GetBlockBlobReference(blobfileName);
+
+                // Check and then Rename local file after file has been sent to Azure Storage Blob
+                Stream stream = new MemoryStream();
+
+                //var klog = blockBlob.DownloadTextAsync().GetAwaiter();
+
+                blockBlob.DownloadToStreamAsync(stream).Wait();
+
+                byte[] buffer = new byte[16 * 1024];
+
+                stream.Position = 0; // Add this line to set the input stream position to 0
+
+                using (MemoryStream ms = new MemoryStream())
+                {
+                    int read;
+
+                    while ((read = stream.Read(buffer, 0, buffer.Length)) > 0)
+                    {
+                        ms.Write(buffer, 0, read);
+                    }
+
+                    document = ms.ToArray();
+
+                    return document;
+                }
+            }
+            catch
+            {
+                return document;
+            }
+        }
+    }
+}
