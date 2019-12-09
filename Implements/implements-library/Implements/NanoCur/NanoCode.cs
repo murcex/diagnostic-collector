a047226af27e5a307e852da07bfb7890324d1728
Robert <vr38dett@live.com>
2019-12-08 16:58:45 -0800

    Implements 20190926 (#63)
    * Update
    
    - Adding draft metadata logic
    - Adding Get Version stubs
    
    * Update
    
    * Update
    
    - Add NanoCur dark logic
    - Add StorageClient with Audit logic

---
+++ Implements/implements-library/Implements/NanoCur/NanoCode.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Implements
+{
+    class NanoCode
+    {
+        private Dictionary<int, string> code = new Dictionary<int, string>();
+
+        public NanoCode()
+        {
+            code.Add(1,"");
+        }
+    }
+}
