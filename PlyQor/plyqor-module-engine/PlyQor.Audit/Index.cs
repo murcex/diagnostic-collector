c1d8cca50942f64ebb7259ca203eda4624b85546
Robert <vr38dett@live.com>
2022-03-27 20:26:59 -0700

    Update

---
+++ PlyQor/plyqor-module-engine/PlyQor.Audit/Index.cs
@@ -0,0 +1,49 @@
+﻿namespace PlyQor.Audit
+{
+    using System;
+    using PlyQor.Audit.Core;
+    using PlyQor.Audit.TestCases.StorageProvider;
+    using PlyQor.Audit.TestCases.PlyManager;
+    using PlyQor.Audit.TestCases.PlyClient;
+    using PlyQor.Engine;
+
+    class Index
+    {
+        static void Main(string[] args)
+        {
+            var testStorageProvider = true;
+
+            var testPlyManager = true;
+
+            var testPlyClient = true;
+
+            Console.WriteLine("");
+
+            Initializer.Execute();
+
+            if (testStorageProvider)
+            {
+                StorageProviderTestProvider.Execute();
+            }
+
+            if (testPlyManager)
+            {
+                PlyManagerTestProvider.Execute();
+            }
+
+            if (testPlyClient)
+            {
+                PlyClientTestProvider.Execute();
+            }
+
+            Console.WriteLine("");
+
+            Console.WriteLine(" \n\r// DataRetention");
+            PlyQorManager.Retention();
+
+            Console.WriteLine("");
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ PlyQor/plyqor-module-engine/PlyQor.Client/Models/ActivityTrace.cs
@@ -0,0 +1,115 @@
+﻿//namespace PlyQor.Models
+//{
+//    using System;
+//    using System.Diagnostics;
+//    using Microsoft.Data.SqlClient;
+
+//    public class ActivityTrace : IDisposable
+//    {
+//        private bool dispose = false;
+
+//        public DateTime Session { get; }
+//        public string Version { get; set; }
+//        public string ActivityId { get; }
+//        public string Operation { get; set; }
+//        public string Code { get; set; }
+//        public bool Status { get; set; }
+//        public double Duration { get; set; }
+//        private string DatabaseConnection { get; set; }
+//        private Stopwatch Tracer { get; set; }
+
+//        public ActivityTrace(string databaseConnectionString, string activityId = null)
+//        {
+//            this.Session = DateTime.UtcNow;
+//            this.Version = "NoContainer";
+//            this.ActivityId = activityId ?? Guid.NewGuid().ToString();
+//            this.Operation = "NoOperation";
+//            this.Code = "OK";
+//            this.Status = true;
+//            this.Tracer = new Stopwatch();
+//            this.Tracer.Start();
+//            this.DatabaseConnection = databaseConnectionString;
+//        }
+
+//        public void AddContainer(string container)
+//        {
+//            this.Version = container;
+//        }
+
+//        public void AddOperation(string operation)
+//        {
+//            this.Operation = operation;
+//        }
+
+//        public void AddCode(string code)
+//        {
+//            code = code.Split(",KEY")[0];
+
+//            this.Code = code;
+//            this.Status = false;
+//        }
+
+//        private void AddLog()
+//        {
+//            try
+//            {
+//                using (var connection = new SqlConnection(DatabaseConnection))
+//                {
+//                    var cmd = new SqlCommand("usp_PlyQor_Trace_Insert", connection);
+
+//                    cmd.CommandType = System.Data.CommandType.StoredProcedure;
+
+//                    cmd.Parameters.AddWithValue("dt_timestamp", Session);
+//                    cmd.Parameters.AddWithValue("nvc_version", Version);
+//                    cmd.Parameters.AddWithValue("nvc_id", ActivityId);
+//                    cmd.Parameters.AddWithValue("nvc_operation", Operation);
+//                    cmd.Parameters.AddWithValue("nvc_code", Code);
+//                    cmd.Parameters.AddWithValue("nvc_status", Status.ToString());
+//                    cmd.Parameters.AddWithValue("i_duration", Duration);
+
+//                    connection.Open();
+
+//                    var reader = cmd.ExecuteReader();
+//                }
+//            }
+//            catch (Exception ex)
+//            {
+//                Console.WriteLine(ex);
+//            }
+//        }
+
+//        protected virtual void Dispose(bool disposing)
+//        {
+//            if (!dispose)
+//            {
+//                if (disposing)
+//                {
+//                    //dispose managed resources
+
+//                    Tracer.Stop();
+//                    this.Duration = Tracer.Elapsed.TotalMilliseconds;
+
+//                    if (this.Status)
+//                    {
+//                        Console.WriteLine($"{this.Operation} @ {this.Duration} ms");
+//                    }
+//                    else
+//                    {
+//                        Console.WriteLine($"<!> {this.Code} <!> {this.Operation} @ {this.Duration} ms");
+//                    }
+
+//                    AddLog();
+//                }
+//            }
+
+//            //dispose unmanaged resources
+//            dispose = true;
+//        }
+
+//        public void Dispose()
+//        {
+//            Dispose(true);
+//            GC.SuppressFinalize(this);
+//        }
+//    }
+//}
