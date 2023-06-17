4639768e807ba4034e21880f4e159c9bf50f9e5c
Robert <20351980+robg374@users.noreply.github.com>
2023-06-16 21:56:51 -0700

    update

---
+++ Sensor/sensor-solution/SensorApp/Core/Startup.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+[assembly: FunctionsStartup(typeof(Sensor.Function.Core.Startup))]
+
+namespace Sensor.Function.Core
+{
+    public class Startup : FunctionsStartup
+    {
+        public override void Configure(IFunctionsHostBuilder builder)
+        {
+            throw new NotImplementedException();
+
+            // var config = ConfiguratorManager.Execute();
+
+            // kiroku
+        }
+    }
+}
