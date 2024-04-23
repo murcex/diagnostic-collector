ab07dc311b81fedc4ce699b054150b452b6697e0
Robert <20351980+robg374@users.noreply.github.com>
2024-04-22 22:28:43 -0700

    Update

---
+++ Implements/implements-solution/Implements.Module.Queue/QueueManager.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Implements.Module.Queue
+{
+	public class QueueManager
+	{
+		private ConcurrentQueue _queue;
+
+		private int _limit;
+
+		private int _duration;
+
+		private Action<object> _action;
+
+		private Action<string> _report;
+
+		public void Enqueue()
+		{
+
+		}
+
+		public void Close()
+		{
+
+		}
+	}
+}
